047f7982fefc4071a1f3195a96464448aa349818
mugikarl <venomcosmic432@gmail.com>
2025-01-20 16:37:58 +0800

    created superuser, and added a env file for credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,11 +102,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "postgres",
-        'USER': "postgres.dhdjsykhydkhafywwmlc",
-        'PASSWORD': "catalyst2024_",
-        'HOST': "aws-0-us-west-1.pooler.supabase.com",
-        'PORT': "5432",
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
